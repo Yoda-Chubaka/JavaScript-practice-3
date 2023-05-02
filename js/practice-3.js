3296f216a609f2b6793cf58f3d14e9549a0445d9
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-05-02 16:02:49 +0300

    Update

--- js/practice-3.js
+++ js/practice-3.js
@@ -272,3 +272,19 @@
 
 
 // Task #20
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+const totalPrice = price * quantity; 
+  for (product of products) {
+    if (product.hasOwnProperty(propName)) {
+
+    }
+  }
+return totalPrice;
+}
