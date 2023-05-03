2e628e5b31a9694ac0483a8715717cb7034f5624
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-05-03 09:02:17 +0300

    Update

--- js/practice-3.js
+++ js/practice-3.js
@@ -344,21 +344,25 @@
 // }
 
 // Task #25
-const forecast = {
-  today: {
-    low: 28,
-    high: 32,
-    icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
-  },
-  tomorrow: {
-    low: 27,
-    high: 31,
-  },
-};
-// Change code below this line
-const {
-  tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
-  todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
-  today: { low, high, icon },
-  tomorrow: { low, high },
-} = forecast;
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+
+// const { today: {low: lowToday, high: highToday, icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",}, tomorrow: { low: lowTomorrow, high: highTomorrow, icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } } = forecast
+
+// Task #26
+// function calculateMeanTemperature(forecast) {
+//   const { today: { low: todayLow, high: todayHigh }, tomorrow: {low: tomorrowLow, high: tomorrowHigh } } = forecast;
+
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+
+// Task #27
